a4820710bd1a693e43adff89879d310a0fd7fc6d
lijixiang <820396305@qq.com>
2017-05-24 20:28:58 +0800

    0.1.7

--- src-docs/components/datePicker.vue
+++ src-docs/components/datePicker.vue
@@ -8,8 +8,6 @@
           </span>
       </xa-cell>
     </xa-cells>
-    <button @click="clearDiv" style="position:fixed;top:100px;left:100px;z-index:100">清空</button>
-    <div id="abcd"></div>
   </div>
 </template>
 <script>
@@ -17,11 +15,6 @@ export default {
   data () {
     return {
       datetime: ''
-    }
-  },
-  methods: {
-    clearDiv () {
-      document.getElementById('abcd').innerHTML = ''
     }
   }
 }
